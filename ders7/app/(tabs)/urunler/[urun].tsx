ce5b5180a4ad8385c73d38a2473719cd2ddb9bf7
bergul <boraergul34@gmail.com>
2024-12-23 18:09:55 +0000

    son

--- ders7/app/(tabs)/urunler/[urun].tsx
+++ ders7/app/(tabs)/urunler/[urun].tsx
@@ -1,12 +1,24 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import axios from 'axios'
 
 const index = () => {
-  const {product}=useLocalSearchParams()
+  const {product}=useLocalSearchParams();
+  const [veri, setVeri] = useState('');
+  useEffect(() => {
+
+    axios.get(`https://dummyjson.com/products/${product}`)
+      .then(res => {
+        const data = res.data;
+        setVeri(data);
+      })  
+
+
+  }, [product]);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'white' }}>
-      <Text>{product}</Text>
+      <Text>{JSON.stringify(veri)}</Text>
     </View>
   )
 }
